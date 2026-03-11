c3e541fb72f773d84972c036bd645cc45dc4fec6
Ivan <ivan.delao@prompt.com.mx>
2026-03-11 13:22:45 -0600

    added card component and fonts

---
+++ app/components/Card.tsx
@@ -0,0 +1,14 @@
+export default function Card(){
+    return(
+         <div className="bg-zinc-800 p-5 w-100 h-50 flex justify-center items-center">
+          
+          <div className="flex flex-col ">
+            <p>Today's Lesson</p>
+          <p>The many uses of get</p>
+          </div>
+          
+          
+        </div>
+    
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Card from "./components/Card";
 
 export default function Home() {
   return (
@@ -11,17 +12,18 @@ export default function Home() {
             <button className="bg-amber-700 rounded-2xl p-3">Start Learning</button>
           </ul>
       </nav>
-     <div className="flex mt-10 justify-between items-center px-20">
+     <div className="flex mt-10 justify-between items-center px-40">
         <div>
-          <h1 className="text-6xl">English that <span className="bold italic text-orange-600 text-2xl">actually</span> makes sense to you </h1>
-          <p>Lessons built for Spanish speakers — no generic content, no confusion. We explain English through your language, not around it.</p>
-        </div>
+          <h1 className="text-6xl w-[50%]">English that <span className="bold italic text-orange-600 text-6xl">actually</span> makes sense to you </h1>
+          <p className="w-[50%] mt-5 text-justify">Lessons built for Spanish speakers — no generic content, no confusion. We explain English through your language, not around it.</p>
+          <div className="flex gap-3 mt-3">
+            <button className="bg-amber-600 rounded-2xl p-3">Browse Lessons</button>
+            <button className="bg-black/55 border-2 border-white rounded-2xl p-3">See how it works</button>
+          </div>
+        </div> 
 
 
-        <div className="border-amber-50 border-solid">
-          card
-        </div>
-    
+       <Card></Card>
 
     </div>
     </main>
