499bcd0ad7b6aa903f3bc72988968a2eb3652119
Ivan <ivan.delao@prompt.com.mx>
2026-03-13 10:56:56 -0600

    added props to Card and styles

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,10 +1,17 @@
-export default function Card(){
+type CardProps = {
+  cardTitle: string;
+  cardBody: string;
+}
+
+export default function Card({cardTitle, cardBody} : CardProps){
     return(
-         <div className="bg-zinc-800 p-5 w-100 h-50 flex justify-center items-center">
+         <div className="bg-zinc-900 p-5 gap-2 w-100 h-50 flex justify-center items-center">
           
-          <div className="flex flex-col ">
-            <p>Today's Lesson</p>
-          <p>The many uses of get</p>
+         <span className="text-2xl">🎯</span>
+
+          <div className="flex flex-col">
+          <p className="font-extralight text-sm">{cardTitle}</p>
+          <p className="font-bold">{cardBody}</p>
           </div>
           
           
--- app/page.tsx
+++ app/page.tsx
@@ -12,18 +12,26 @@ export default function Home() {
             <button className="bg-amber-700 rounded-2xl p-3">Start Learning</button>
           </ul>
       </nav>
+
+    {/* <div className="flex mt-10 w-5xl justify-between items-center px-40 bg-zinc-800 text-orange-600">
+      <button className="rounded-2xl">Para hispanohablantes</button>
+    </div> */}
+
+       <button className="rounded-2xl ml-20 md:ml-40 mt-5 bg-zinc-800 text-orange-600 p-2">Para hispanohablantes</button>
+
+  
      <div className="flex mt-10 justify-between items-center px-40">
         <div>
           <h1 className="text-6xl w-[50%]">English that <span className="bold italic text-orange-600 text-6xl">actually</span> makes sense to you </h1>
           <p className="w-[50%] mt-5 text-justify">Lessons built for Spanish speakers — no generic content, no confusion. We explain English through your language, not around it.</p>
           <div className="flex gap-3 mt-3">
-            <button className="bg-amber-600 rounded-2xl p-3">Browse Lessons</button>
-            <button className="bg-black/55 border-2 border-white rounded-2xl p-3">See how it works</button>
+            <button className="bg-amber-600 rounded-2xl p-3 hover:cursor-pointer">Browse Lessons</button>
+            <button className="bg-black/55 border-2 border-white rounded-2xl p-3 hover:cursor-pointer">See how it works</button>
           </div>
         </div> 
 
 
-       <Card></Card>
+       <Card cardTitle="Today's Lesson" cardBody="The many uses of 'get'"></Card>
 
     </div>
     </main>
