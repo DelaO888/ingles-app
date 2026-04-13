07f5ab8ed5a0cfcb3bdfe141b3cd2db913228331
Ivan <ivan.delao@prompt.com.mx>
2026-04-13 16:42:50 -0600

    correctly added icon inside input

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -20,7 +20,10 @@ export default function LoginPage(){
         <div className="flex flex-col mx-auto mt-20 w-80 gap-4">
                 <h1 className="text-2xl font-bold">Login</h1>
                 <input type="email" className="bg-zinc-800 p-3 rounded-lg" placeholder="Email" onChange={e => setEmail(e.target.value)} />
-             <div className="block"><input type={showPassword ? "text" : "password"} className="bg-zinc-800 p-3 rounded-lg" placeholder="Password" onChange={e => setPassword(e.target.value)} /> <button onClick={handleShowPassword}>{showPassword ? "🙉"  : "🙈"}</button> </div>   
+             <div className="relative w-full">
+                    <input type={showPassword ? "text" : "password"} className="bg-zinc-800 w-full p-3 rounded-lg pr-10" placeholder="Password" onChange={e => setPassword(e.target.value)} /> 
+                    <button className="absolute right-3 top-1/2 -translate-y-1/2" onClick={handleShowPassword}>{showPassword ? "🙉"  : "🙈"}</button> 
+                </div>   
                   <button className="bg-amber-700 p-3 rounded-2xl" onClick={handleLogin}>Login</button>
                   <Link href="/register" className="bg-amber-700 p-3 rounded-2xl" >OR Register</Link>
         </div>
